6fad8f785f56d867bb5223d334909379141e54ff
Djimra NGARLEITA <dngarleita@gmail.com>
2024-04-18 03:33:53 +0300

    chore: Initial commit, task 3 and 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- Create a new database with the name 'hbnb_test_db'
+-- Create a new user 'hbnb_dev' with different privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
